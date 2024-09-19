a67a297b037fa927295a59558750ca505c5a3b4c
RtrMmmt <ryotaro.manmoto@gmail.com>
2024-09-20 07:55:06 +0900

    fix

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,16 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+local lspconfig = require('lspconfig')
+
+lspconfig.clangd.setup({
+    cmd = { "clangd" },
+    on_attach = function(client, bufnr)
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    end,
+    flags = {
+        debounce_text_changes = 150,
+    },
+    -- extraArgs にインクルードパスを指定
+    init_options = {
+        compilationDatabaseDirectory = ".",
+        extraArgs = { "-I/usr/local/Cellar/open-mpi/5.0.3_1/include" },
+    }
+})
